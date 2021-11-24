c36aa30dfe005db6000b150353756d64cf75082d
Ben W <merovex@hey.com>
2021-11-24 17:04:19 -0500

    restyle, simplified

--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -17,9 +17,10 @@ module.exports = {
         light: {
           css: {
             'background': 'none',
-            color: theme('colors.black'),
+            color: theme('colors.shade.dark'),
             a: {
-              color: theme('colors.blue.500'),
+              color: theme('colors.brand'),
+              "font-family": theme('font.sans'),
               '&:visited': {
                 color: theme('colors.violet.500'),
               },
@@ -32,32 +33,33 @@ module.exports = {
         },
         dark: {
           css: {
-            color: theme('colors.gray.50'),
+            color: theme('colors.shade.light'),
             a: {
-              color: theme('colors.blue.300'),
+              color: theme('colors.brand.light'),
+              fontFamily: theme('font.sans'),
               '&:hover': {
-                color: theme('colors.blue.100'),
+                color: theme('colors.accent'),
               },
               '&:visited': {
-                color: theme('colors.violet.300'),
+                color: theme('colors.accent'),
               }
             },
             'p, li': {
-              color: theme('colors.gray.100')
+              color: theme('colors.shade.light')
             },
             'p+p': {
               'text-indent': '2rem',
               'margin': 0,
             },
             blockquote: {
-              color: theme('colors.gray.200'),
+              color: theme('colors.shade.light'),
             },
             strong: {
-              color: theme('colors.yellow.300'),
+              color: theme('colors.warning'),
               'font-weight': 'bold'
             },
             'h1,h2,h3,h4,h5': {
-              color: theme('colors.brand.200')
+              color: theme('colors.accent.light')
             },
           },
         }
@@ -68,7 +70,7 @@ module.exports = {
       'serif': ["MerovexSerif", 'Cambria', 'Georgia', "Times New Roman", 'Times', 'serif'],
       'display': ["MerovexDisplay", "MerovexSans", '-apple-system', 'system-ui', "Segoe UI", 'Roboto', 'Lato', 'Helvetica', 'Arial', 'sans-serif']
     },
-    colors: { // https://tailwindpalette.jurs.me/
+    xcolors: { // https://tailwindpalette.jurs.me/
       xbrand: {
         '50': '#fcfbf9',
         '100': '#f9f1e6',
@@ -210,6 +212,50 @@ module.exports = {
         '700': '#404F62',
         '800': '#2A3440',
         '900': '#14191F'
+      }
+    },
+    colors: {
+      brand: {
+        DEFAULT: '#406EA6',
+        light: '#98B6D7', // WCAG 2.0 7:0
+        dark: '#31537D' // WCAG 2.0 7:0
+      },
+      accent: {
+        lightest: '#91B5CA', // WCAG 2.0 7:0
+        light: '#6D9F89',
+        dark: '#7B4B42',
+        darkest: '#70473E', // WCAG 2.0 7:0
+      },
+      shade: {
+        lighter: '#fcfcfc',
+        light: '#F3F1F2', // white
+        dark: '#202434', // black
+        darker: '#11131c',
+      },
+      info: {
+        light: '#FeF1F2',
+        dark: '#202434',
+      },
+      success: {
+        light: '#499C6D',
+        DEFAULT: '#499B6B',
+        dark: '#397954',
+      },
+      warning: {
+        light: '#C68B33',
+        DEFAULT: '#C68B33',
+        dark: '#8E6425',
+      },
+      danger: {
+        light: '#F55347',
+        DEFAULT: '#F44336',
+        dark: '#DA190B'
+      },
+      amazon: {
+        DEFAULT: '#FF9900'
+      },
+      noble: {
+        DEFAULT: '#366251'
       }
     }
   }
